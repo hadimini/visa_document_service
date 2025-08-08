dce4d9716899285da65ad384094e009e35ca2733
Hadi Mini <hadimini@gmail.com>
2025-08-08 11:37:52 +0300

    refactor db connection

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,12 +1,34 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core import config
+from app.db import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # create the tables in the database if they don't exist
+    print("Server is starting...")
+    await init_db()
+    yield
+    print("Server has been stopped...")
 
 
 def get_application() -> FastAPI:
     app = FastAPI(
         title=config.PROJECT_NAME,
         version=config.PROJECT_VERSION,
+        lifespan=lifespan
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     return app
---
+++ backend/app/db.py
@@ -0,0 +1,30 @@
+import os
+
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine, SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.core.config import DATABASE_URL
+
+
+DB_URL = f"{DATABASE_URL}_test" if os.environ.get("TESTING") else DATABASE_URL
+
+engine = AsyncEngine(
+    create_engine(DB_URL, echo=False, future=True)
+)
+
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+    async with Session() as session:
+        yield session
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
