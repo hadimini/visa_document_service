eace62f483fcd01f49f271fab9e15293b0ffa60e
Hadi Mini <hadimini@gmail.com>
2025-08-09 10:53:05 +0300

    update

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,18 +1,11 @@
 from fastapi import APIRouter, Depends
-# from sqlalchemy import
-from pydantic import TypeAdapter
-from sqlalchemy import select, insert
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models.users import User
 from app.api.dependencies.db import get_db, get_repository
 from app.database.repositories.users import UsersRepository
 from app.schemas.user import UserPublic
 
 
 router = APIRouter()
-
-# ta = TypeAdapter(list[UserBase])
 
 
 @router.get("/", response_model=list[UserPublic], name="users:user-list")
