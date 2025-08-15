bdf3c1418d05179abf9c141d2db9f7f4ef4d7513
Hadi Mini <hadimini@gmail.com>
2025-08-15 19:05:16 +0300

    add_dummy_task

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -1,4 +1,4 @@
 def write_notification(email: str, message=""):
-    with open("log.txt", mode="w") as email_file:
-        content = f"notification for {email}: {message}"
+    with open("log.txt", mode="a") as email_file:
+        content = f"\nnotification for {email}: {message}"
         email_file.write(content)
