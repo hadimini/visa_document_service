bad12dcee3cc3273dc5377b055a31320c5a7a77e
Hadi Mini <hadimini@gmail.com>
2025-08-17 12:29:02 +0300

    Add entry_logs to user model

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy import String, Integer, Boolean, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
 from app.database.db import Base
@@ -20,6 +20,8 @@ class User(Base):
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    entry_logs: Mapped[list["LogEntry"]] = relationship(back_populates="user")
 
     def __repr__(self):
         return f"<User {self.id}>"
