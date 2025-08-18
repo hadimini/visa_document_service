247b4791f570d43a295374ddcda6f3a42e2f13c7
Hadi Mini <hadimini@gmail.com>
2025-08-18 15:13:41 +0300

    User.role set nullable = False

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -25,7 +25,7 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False, index=True)
     email_verified: Mapped[bool] = mapped_column(Boolean, default=False)
     password: Mapped[str] = mapped_column(String(100), nullable=False)
-    role: Mapped[str] = mapped_column(ChoiceType(ROLE_CHOICES), nullable=True, server_default=ROLE_USER)
+    role: Mapped[str] = mapped_column(ChoiceType(ROLE_CHOICES), nullable=False, server_default=ROLE_USER)
     salt: Mapped[str] = mapped_column(String(100), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
