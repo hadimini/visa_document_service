c5d74fabe45012ac7535b37b7f9cdfd83e44a8f3
Hadi Mini <hadimini@gmail.com>
2025-08-22 14:08:47 +0300

    Update typehints

--- backend/app/schemas/client.py
+++ backend/app/schemas/client.py
@@ -1,20 +1,21 @@
-from typing import Annotated, Literal, Optional
+from typing import Annotated, Optional, Union
 
 from pydantic import StringConstraints
 
+from app.models.clients import Client
 from app.schemas.core import CoreSchema, IDSchemaMixin
 
 
 class ClientBaseSchema(CoreSchema):
     tariff_id: Optional[int] = None
     name: Optional[str] = None
-    type: Literal["individual", "legal"] | None = None
+    type: Union[Client.TYPE_INDIVIDUAL, Client.TYPE_LEGAL, None] = None
 
 
 class ClientCreateSchema(CoreSchema):
     tariff_id: int
     name: Annotated[str, StringConstraints(min_length=3, max_length=100, pattern="^[a-zA-Z0-9 ]+$")]
-    type: Literal["individual", "legal"]
+    type: Union[Client.TYPE_INDIVIDUAL, Client.TYPE_LEGAL]
 
 
 class ClientInDBSchema(IDSchemaMixin):
