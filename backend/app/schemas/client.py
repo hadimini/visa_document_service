bb2d2634da938bee5f9019d8ef3cd130286317b3
Hadi Mini <hadimini@gmail.com>
2025-08-22 14:34:31 +0300

    Replace Union with Literal

--- backend/app/schemas/client.py
+++ backend/app/schemas/client.py
@@ -1,21 +1,20 @@
-from typing import Annotated, Optional, Union
+from typing import Annotated, Literal, Optional
 
 from pydantic import StringConstraints
 
-from app.models.clients import Client
 from app.schemas.core import CoreSchema, IDSchemaMixin
 
 
 class ClientBaseSchema(CoreSchema):
     tariff_id: Optional[int] = None
     name: Optional[str] = None
-    type: Union[Client.TYPE_INDIVIDUAL, Client.TYPE_LEGAL, None] = None
+    type: Literal["individual", "legal"] | None = None
 
 
 class ClientCreateSchema(CoreSchema):
     tariff_id: int
     name: Annotated[str, StringConstraints(min_length=3, max_length=100, pattern="^[a-zA-Z0-9 ]+$")]
-    type: Union[Client.TYPE_INDIVIDUAL, Client.TYPE_LEGAL]
+    type: Literal["individual", "legal"]
 
 
 class ClientInDBSchema(IDSchemaMixin):
