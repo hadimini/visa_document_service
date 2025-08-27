394ce88ae38ca4aa90dfe02bd8ad5144a1952703
Hadi Mini <hadimini@gmail.com>
2025-08-27 09:49:18 +0300

    Add tags to auth router

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -8,5 +8,5 @@ from app.api.routes.individual import router as individual_router
 router = APIRouter()
 
 router.include_router(admin_router, prefix="/admin")
-router.include_router(auth_router, prefix="/auth")
+router.include_router(auth_router, prefix="/auth", tags=["auth"])
 router.include_router(individual_router, prefix="/individual")
