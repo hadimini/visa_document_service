0f14316800ae397557e342a7aa4f860a2c1f99e0
Hadi Mini <hadimini@gmail.com>
2025-08-28 16:33:23 +0300

    Import AbstractBaseModel

--- backend/app/models/visa_types.py
+++ backend/app/models/visa_types.py
@@ -3,13 +3,13 @@ from typing import TYPE_CHECKING
 from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.database.db import Base
+from app.models.base import AbstractBaseModel
 
 if TYPE_CHECKING:
     from app.models.country_visas import CountryVisa
 
 
-class VisaType(Base):
+class VisaType(AbstractBaseModel):
     __tablename__ = "visa_types"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
