56899c8838a3890fd878fa69a409350a17e54b05
Hadi Mini <hadimini@gmail.com>
2025-09-04 14:35:16 +0300

    Add fields

--- backend/app/models/services.py
+++ backend/app/models/services.py
@@ -3,32 +3,34 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database.custom_types import ChoiceType
 from app.models.base import Base
-from app.models.mixins import CreatedAtMixin, UpdatedAtMixin
+from app.models.mixins import CreatedAtMixin, UpdatedAtMixin, ArchivedAtMixin
 
 
-class Service(CreatedAtMixin, UpdatedAtMixin, Base):
+class Service(ArchivedAtMixin, CreatedAtMixin, UpdatedAtMixin, Base):
     __tablename__ = "services"
 
-    FEE_TYPE_CENTER = "center"
     FEE_TYPE_CONSULAR = "consular"
-    FEE_TYPE_DELIVERY = "delivery"
     FEE_TYPE_GENERAL = "general"
-    FEE_TYPE_INVITATION = "invitation"
-    FEE_TYPE_SERVICE_PROVIDER = "provider"
 
     FEE_TYPE_CHOICES = (
-        (FEE_TYPE_CENTER, "Center"),
         (FEE_TYPE_CONSULAR, "Consular"),
-        (FEE_TYPE_DELIVERY, "Delivery"),
         (FEE_TYPE_GENERAL, "General"),
-        (FEE_TYPE_INVITATION, "Invitation"),
-        (FEE_TYPE_SERVICE_PROVIDER, "Service Provider"),
     )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String)
     fee_type: Mapped[str] = mapped_column(ChoiceType(FEE_TYPE_CHOICES))
     country_id: Mapped[int] = mapped_column(Integer, ForeignKey("countries.id", ondelete="CASCADE"))
+    duration_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("durations.id", ondelete="CASCADE"), nullable=True
+    )
+    visa_type_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("visa_types.id", ondelete="CASCADE"), nullable=True
+    )
+    urgency_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("urgencies.id", ondelete="CASCADE"), nullable=True
+    )
+
 
     # TODO
     # visa_type, duration, urgency
