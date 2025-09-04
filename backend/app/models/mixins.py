8c6236006c15af7035321e6cb9c67ced89e47a54
Hadi Mini <hadimini@gmail.com>
2025-09-04 14:35:01 +0300

    ArchivedAtMixin

--- backend/app/models/mixins.py
+++ backend/app/models/mixins.py
@@ -13,6 +13,10 @@ class UpdatedAtMixin:
     updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
 
 
+class ArchivedAtMixin:
+    archived_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+
 class CompletedAtMixin:
     completed_at: Mapped[datetime] = mapped_column(
         DateTime,
